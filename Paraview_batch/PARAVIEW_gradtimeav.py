9ced79af158951bbae4574b6107285cfeb0141e1
jedi <bhanu2204@gmail.com>
2023-04-18 17:57:56 +0200

    updated the repo with the paraview batch script for time averaging velocity gradient on a boundary or internal Mesh

---
+++ Paraview_batch/PARAVIEW_gradtimeav.py
@@ -0,0 +1,30 @@
+from paraview.simple import *
+paraview.simple._DisableFirstRenderCameraReset()
+
+## For this script, an openFOAM case is needed. And also only the folders required for time averaging should be inside the case folder in addition
+## to 0/ constant/ and system/ 
+
+##update the file path here to the case folder and a dummy .OpenFOAM extension file. 
+airfoilOpenFOAM = OpenFOAMReader(FileName='/home/bhanu/airfoil/airfoil.OpenFOAM')
+
+animationScene1 = GetAnimationScene()
+animationScene1.UpdateAnimationUsingDataTimeSteps()
+animationScene1.GoToLast()
+
+##update the boundary name or if averaging is needed for internalMesh
+airfoilOpenFOAM.MeshRegions = ['AirfoilPS']
+
+##update the field to be averaged. 
+airfoilOpenFOAM.CellArrays = ['grad(U)']
+
+temporalStatistics1 = TemporalStatistics(Input=airfoilOpenFOAM)
+temporalStatistics1.ComputeMinimum = 0
+temporalStatistics1.ComputeMaximum = 0
+temporalStatistics1.ComputeStandardDeviation = 0
+
+plotData1 = PlotData(Input=temporalStatistics1)
+
+##update the folder where the time averaged csv file should be located at
+SaveData('/home/bhanu/airfoil/PSgradscript.csv', proxy=plotData1, Precision=8)
+
+
