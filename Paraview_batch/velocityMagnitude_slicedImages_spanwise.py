fde6220e8010d4a4d4e0dfdc49ca59e5093c13d5
CFDcodesBPS <bhanu2204@gmail.com>
2023-04-20 15:20:45 +0200

    To compute the velocity magnitude sliced images at a chosen streamwise location and iterated to run in all time step folders, for OpenFOAM cases

--- Paraview_batch/velocityMagnitude_slicedImages_spanwise.py
+++ Paraview_batch/velocityMagnitude_slicedImages_spanwise.py
@@ -1,6 +1,5 @@
 ## This is a python script compatible with Paraview batch
-## This script generates two images of Reynolds Stress at a sliced location of X = 0.95 Chord lengths and X = 2 chord lengths, 
-## YZ plane images or spanwise crosssection.
+## This script generates velocity Magnitude at a sliced location of X = 0.95 Chord lengths, YZ plane images or spanwise crosssection.
 
 from paraview.simple import *
 paraview.simple._DisableFirstRenderCameraReset()
@@ -11,11 +10,12 @@ paraview.simple._DisableFirstRenderCameraReset()
 ##update the file path here to the case folder and a dummy .OpenFOAM extension file.
 airfoilOpenFOAM = OpenFOAMReader(FileName='/home/bhanu/Airfoil/baseline/baseline.OpenFOAM')
 airfoilOpenFOAM.MeshRegions = ['internalMesh']
-airfoilOpenFOAM.CellArrays = ['UPrime2Mean']
+airfoilOpenFOAM.CellArrays = ['U']
 
 
 animationScene1 = GetAnimationScene()
 animationScene1.UpdateAnimationUsingDataTimeSteps()
+
 ##all the time step folder names are arranged in list and saved in time list
 time = list(airfoilOpenFOAM.TimestepValues)
 print time
@@ -24,16 +24,16 @@ renderView1 = GetActiveViewOrCreate('RenderView')
 # uncomment following to set a specific view size
 renderView1.ViewSize = [1544, 805]
 
-uLUT = GetColorTransferFunction('UPrime2Mean')
-uPWF = GetOpacityTransferFunction('UPrime2Mean')
+uLUT = GetColorTransferFunction('U')
+uPWF = GetOpacityTransferFunction('U')
 
 airfoilOpenFOAMDisplay = Show(airfoilOpenFOAM, renderView1)
-airfoilOpenFOAMDisplay.ColorArrayName = ['POINTS', 'UPrime2Mean']
+airfoilOpenFOAMDisplay.ColorArrayName = ['POINTS', 'U']
 airfoilOpenFOAMDisplay.LookupTable = uLUT
 
 renderView1.ResetCamera()
 
-ColorBy(airfoilOpenFOAMDisplay, ('POINTS', 'UPrime2Mean'))
+ColorBy(airfoilOpenFOAMDisplay, ('POINTS', 'U'))
 
 # create a new 'Slice'
 slice1 = Slice(Input=airfoilOpenFOAM)
@@ -43,7 +43,7 @@ slice1.SliceType.Origin = [0.95, 0.0, -0.5]
 
 # show data in view
 slice1Display = Show(slice1, renderView1)
-slice1Display.ColorArrayName = ['POINTS', 'UPrime2Mean']
+slice1Display.ColorArrayName = ['POINTS', 'U']
 slice1Display.LookupTable = uLUT
 
 # hide data in view
@@ -55,7 +55,7 @@ renderView1.ResetCamera()
 Hide3DWidgets(proxy=slice1)
 
 # set scalar coloring
-ColorBy(slice1Display, ('POINTS', 'UPrime2Mean'))
+ColorBy(slice1Display, ('POINTS', 'U'))
 
 # show color bar/color legend
 slice1Display.SetScalarBarVisibility(renderView1, True)
@@ -68,25 +68,15 @@ annotateTimeFilter1Display = Show(annotateTimeFilter1, renderView1)
 annotateTimeFilter1Display.WindowLocation = 'UpperCenter'
 annotateTimeFilter1.Format = 'Time: %9.9f'
 
-##Looping the script to run across all time folders in OpenFOAM case and generate Reynolds Stress mag. images
-for x in xrange(0, (len(time)/10)):
-	animationScene1.AnimationTime = time[10*x]
+## Looping the script to run across all time folders in OpenFOAM case and generate Velocity mag. images
+for x in xrange(0, len(time)):
+	animationScene1.AnimationTime = time[x]
 	uLUT.RescaleTransferFunction(0.0, 1.0)
 	uPWF.RescaleTransferFunction(0.0, 1.0)
-	slice1.SliceType.Origin = [0.95, 0.0, -0.5]
         ##update camera view parameters as required
-        renderView1.CameraPosition = [2.9139260961387206, 0.10519199243495736, -0.6625439234682522]
+	renderView1.CameraPosition = [2.9139260961387206, 0.10519199243495736, -0.6625439234682522]
 	renderView1.CameraFocalPoint = [-43.44374845765281, 0.10519199243495736, -0.6625439234682522]
 	renderView1.CameraViewUp = [0.0, 0.9999999202560829, -0.00039935927177369053]
 	renderView1.CameraParallelScale = 11.998249061185744
-        ##folder to save Reynolds Stress images
-	SaveScreenshot('/home/bhanu/Airfoil/baseline/yzplane_R/y095_'+str(x)+'.png', magnification=1, quality=100, view=renderView1)
-	slice1.SliceType.Origin = [2, 0.0, -0.5]
-        ##another camera angle for second set of Reynolds stress images
-        renderView1.CameraPosition = [2.9139260961387206, 0.10519199243495736, -0.6625439234682522]
-        renderView1.CameraFocalPoint = [-43.44374845765281, 0.10519199243495736, -0.6625439234682522]
-        renderView1.CameraViewUp = [0.0, 0.9999999202560829, -0.00039935927177369053]
-        renderView1.CameraParallelScale = 11.998249061185744
-        SaveScreenshot('/home/bhanu/Airfoil/baseline/yzplane_R/y2_'+str(x)+'.png', magnification=1, quality=100, view=renderView1)
-
-
+        ##folder to save velocity images
+	SaveScreenshot('/home/bhanu/Airfoil/baseline/yzplane/y095_'+str(x)+'.png', magnification=1, quality=100, view=renderView1)
